93ffea26242b1b9e6b47ed88b5e3f19d6999531e
EriyaDev <eriyagamedev@gmail.com>
2026-03-16 22:22:26 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import HeroSection from '@/components/sections/HeroSection.vue'
 import ImpactSection from '@/components/sections/ImpactSection.vue'
+import PurposeSection from '@/components/sections/PurposeSection.vue'
 </script>
 
 <template>
   <HeroSection />
   <ImpactSection />
+  <PurposeSection />
   <div class="h-[150vh] bg-blue-100"></div>
 </template>
