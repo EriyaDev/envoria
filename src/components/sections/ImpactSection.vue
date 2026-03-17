abd9ee88f4db0541e8b7133e451a91c20521fc8f
EriyaDev <eriyagamedev@gmail.com>
2026-03-17 16:03:41 +0800

    fix: responsive impact section

--- src/components/sections/ImpactSection.vue
+++ src/components/sections/ImpactSection.vue
@@ -2,7 +2,7 @@
 
 <template>
   <section class="bg-third-color">
-    <div class="container mx-auto py-10 px-3 md:px-0">
+    <div class="section-container mx-auto py-10 md:px-0">
       <div class="flex flex-col">
         <span
           class="flex flex-row items-center w-fit px-3 py-1 bg-black text-white text-small rounded-full font-semibold border border-border-color"
@@ -10,7 +10,7 @@
           <div class="w-2 h-2 bg-accent-color rounded-full mr-2"></div>
           Our Impact</span
         >
-        <h2 class="medium-heading font-medium text-primary-color mt-10 lg:max-w-[20%]">
+        <h2 class="medium-heading font-medium text-primary-color mt-10 xl:max-w-[20%]">
           Numbers that speak for the <span class="text-accent-color">planet</span>.
         </h2>
         <p class="text-body text-primary-color/55 mt-3 md:max-w-[70%]">
@@ -25,30 +25,30 @@
         >
           <h3 class="text-display font-bold text-accent-color">98%</h3>
           <p class="text-body font-semibold text-primary-color/55">Customer Satisfaction</p>
-          <p class="text-small text-primary-color/55">
+          <p class="text-small text-primary-color/55 line-clamp-2">
             Volunteers who return again and again to take action with us.
           </p>
         </div>
         <div class="flex flex-col gap-2 bg-fourth-color px-8 py-12 md:rounded-lg">
           <h3 class="text-display font-bold text-accent-color">98%</h3>
           <p class="text-body font-semibold text-primary-color/55">Customer Satisfaction</p>
-          <p class="text-small text-primary-color/55">
+          <p class="text-small text-primary-color/55 line-clamp-2">
             Volunteers who return again and again to take action with us.
           </p>
         </div>
         <div class="flex flex-col gap-2 bg-fourth-color px-8 py-12 md:rounded-lg lg:rounded-none">
           <h3 class="text-display font-bold text-accent-color">98%</h3>
           <p class="text-body font-semibold text-primary-color/55">Customer Satisfaction</p>
-          <p class="text-small text-primary-color/55">
+          <p class="text-small text-primary-color/55 line-clamp-2">
             Volunteers who return again and again to take action with us.
           </p>
         </div>
         <div
-          class="flex flex-col gap-2 bg-fourth-color px-8 py-12 rounded-t-lg md:rounded-lg lg:rounded-r-lg"
+          class="flex flex-col gap-2 bg-fourth-color px-8 py-12 rounded-b-lg md:rounded-lg lg:rounded-r-lg"
         >
           <h3 class="text-display font-bold text-accent-color">98%</h3>
           <p class="text-body font-semibold text-primary-color/55">Customer Satisfaction</p>
-          <p class="text-small text-primary-color/55">
+          <p class="text-small text-primary-color/55 line-clamp-2">
             Volunteers who return again and again to take action with us.
           </p>
         </div>
