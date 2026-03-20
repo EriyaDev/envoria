63c7c9ae66d4248004795fb58c81d121df281369
kusumadeveloper01 <idabagustrikusumadewa@gmail.com>
2026-03-20 14:28:35 +0800

    update

--- src/components/sections/UpcomingProgramSection.vue
+++ src/components/sections/UpcomingProgramSection.vue
@@ -29,10 +29,12 @@ import 'swiper/css/pagination'
                     </p>
                 </div>
 
-                <ButtonSecondary to="/programs" class="hidden md:flex">
-                    <p class="text-body font-medium">See All Programs</p>
-                    <img src="../../assets/icons/home/arrow-white.svg" alt="" />
-                </ButtonSecondary>
+                <div class="hidden md:flex">
+                    <ButtonSecondary to="/programs" class="">
+                        <p class="text-body font-medium">See All Programs</p>
+                        <img src="../../assets/icons/home/arrow-white.svg" alt="" />
+                    </ButtonSecondary>
+                </div>
             </div>
 
             <!-- versi pake slider -->
@@ -207,7 +209,8 @@ import 'swiper/css/pagination'
 
                         <div class="flex flex-col gap-2">
                             <h2 class="small-heading font-medium">Borneo Forest Reforestation 2026</h2>
-                            <p class="text-small text-text-third-color">A program to plant 10,000 endemic trees
+                            <p class="text-small text-text-third-color line-clamp-2">A program to plant 10,000 endemic
+                                trees
                                 in critical lands of
                                 Central
                                 Kalimantan with
@@ -249,38 +252,34 @@ import 'swiper/css/pagination'
                 <div class="grid grid-cols-1 md:grid-cols-10 p-5 rounded-lg bg-primary-color gap-5">
                     <div class="md:col-span-6 gap-10 flex flex-col order-last md:order-first">
                         <span
-                            class="flex flex-row items-center w-fit px-3 py-1 bg-accent-color/16 text-text-primary-color border border-border-color/55 text-small rounded-full font-semibold">
-                            <div class="w-2 h-2 bg-accent-color rounded-full mr-2"></div>
-                            Active Now
+                            class="flex flex-row items-center w-fit px-3 py-1 bg-[#FFBB5B]/16 text-text-primary-color border border-[#61592B]/13 text-small rounded-full font-semibold">
+                            <div class="w-2 h-2 bg-[#8B1C00] rounded-full mr-2"></div>
+                            Coming Soon
                         </span>
 
                         <div class="flex flex-col gap-2">
-                            <h2 class="small-heading font-medium">Borneo Forest Reforestation 2026</h2>
-                            <p class="text-small text-text-third-color">A program to plant 10,000 endemic trees
-                                in critical lands of
-                                Central
-                                Kalimantan with
-                                local communities.</p>
+                            <h2 class="small-heading font-medium">Clean-Up Pantai Bali 2026</h2>
+                            <p class="text-small text-text-third-color line-clamp-2">A mass beach cleanup took place in
+                                five
+                                locations in Bali. The goal was to remove 3 tons of plastic waste from the coast.</p>
                         </div>
 
                         <div class="border border-dashed border-[#8E8E8E] flex flex-col gap-2 p-3 rounded-lg">
                             <span class="flex items-center gap-2">
                                 <img src="../../assets/icons/home/calendar.svg" alt="" />
-                                <span class="text-small text-text-third-color font-medium">15 Mar - 30 Jun
-                                    2026</span>
+                                <span class="text-small text-text-third-color font-medium">20 Mar - 30 July 2026</span>
                             </span>
 
                             <div class="w-full">
                                 <div
                                     class="w-full h-3 bg-[#DBDBDB] rounded-full overflow-hidden border border-gray-200">
-                                    <div class="h-full bg-accent-color rounded-full" style="width: 87%"></div>
+                                    <div class="h-full bg-accent-color rounded-full" style="width: 0%"></div>
                                 </div>
                             </div>
 
                             <span class="flex items-center justify-between gap-2">
-                                <span class="text-small text-text-third-color font-medium">6.700 / 10.000
-                                    Tree</span>
-                                <span class="text-small text-text-third-color font-medium">67%</span>
+                                <span class="text-small text-text-third-color font-medium">0 / 100 Units</span>
+                                <span class="text-small text-text-third-color font-medium">0%</span>
                             </span>
                         </div>
 
@@ -290,17 +289,18 @@ import 'swiper/css/pagination'
                         </ButtonPrimary>
                     </div>
                     <div class="md:col-span-4 h-52 md:h-full w-full order-first md:order-last">
-                        <img src="../../assets/images/upcoming-program/image-1.svg" alt=""
+                        <img src="../../assets/images/upcoming-program/image-2.svg" alt=""
                             class="h-full w-full object-cover rounded-lg">
                     </div>
                 </div>
             </div>
 
-            <ButtonSecondary to="/programs" class="md:hidden mt-10 mx-auto flex">
-                <p class="text-body font-medium">See All Programs</p>
-                <img src="../../assets/icons/home/arrow-white.svg" alt="" />
-            </ButtonSecondary>
-
+            <div class="md:hidden mt-10 justify-center flex">
+                <ButtonSecondary to="/programs" class="">
+                    <p class="text-body font-medium">See All Programs</p>
+                    <img src="../../assets/icons/home/arrow-white.svg" alt="" />
+                </ButtonSecondary>
+            </div>
         </div>
     </div>
 </template>
