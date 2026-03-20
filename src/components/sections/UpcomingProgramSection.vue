ebff07446b96ccdb646669cf7e8c609fe6debafc
kusumadeveloper01 <idabagustrikusumadewa@gmail.com>
2026-03-20 11:08:43 +0800

    tes

--- src/components/sections/UpcomingProgramSection.vue
+++ src/components/sections/UpcomingProgramSection.vue
@@ -5,9 +5,9 @@
         <div class="section-container mx-auto">
             <div class="flex flex-col gap-3 items-start">
                 <span
-                    class="flex flex-row items-center w-fit px-3 py-1 bg-black/5 text-text-primary-color border border-border-color/55 text-small rounded-full font-semibold">
-                    <div class="w-2 h-2 bg-accent-color rounded-full mr-2"></div>
-                    Our Purpose
+                    class="flex flex-row items-center w-fit px-3 py-1 bg-black/5 text-text-primary-color border border-[#2D612B]/13 text-small rounded-full font-semibold">
+                    <div class="w-2 h-2 bg-[#038B00] rounded-full mr-2"></div>
+                    Upcoming Programs
                 </span>
                 <h2
                     class="medium-heading font-medium text-text-primary-color mt-10 text-start md:max-w-[70%] xl:max-w-[37%]">
@@ -18,6 +18,22 @@
                     Every program, every volunteer, and every tree we plant is living proof that collective
                     action reshapes the environment one small step at a time.
                 </p>
+            </div>
+
+            <div class="grid grid-cols-2 gap-10 mt-10">
+                <div class="grid grid-cols-10 gap-3">
+                    <div class="col-span-6 bg-primary-color p-5 rounded-lg gap-10 flex flex-col">
+                        <span
+                            class="flex flex-row items-center w-fit px-3 py-1 bg-accent-color/16 text-text-primary-color border border-border-color/55 text-small rounded-full font-semibold">
+                            <div class="w-2 h-2 bg-accent-color rounded-full mr-2"></div>
+                            Active Now
+                        </span>
+
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
