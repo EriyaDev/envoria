35f81e3429351a3dbc17215090d59ccb4293114b
kusumadeveloper01 <idabagustrikusumadewa@gmail.com>
2026-03-20 10:50:28 +0800

    upcoming program section

---
+++ src/components/sections/UpcomingProgramSection.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+    <div class="bg-[#f5f5f5] py-10">
+        <div class="section-container mx-auto">
+            <div class="flex flex-col gap-3 items-start">
+                <span
+                    class="flex flex-row items-center w-fit px-3 py-1 bg-black/5 text-text-primary-color border border-border-color/55 text-small rounded-full font-semibold">
+                    <div class="w-2 h-2 bg-accent-color rounded-full mr-2"></div>
+                    Our Purpose
+                </span>
+                <h2
+                    class="medium-heading font-medium text-text-primary-color mt-10 text-start md:max-w-[70%] xl:max-w-[37%]">
+                    Our Real-World <br> Action
+                    <span class="text-text-primary-color/55">Programs</span>
+                </h2>
+                <p class="text-body font-medium text-text-third-color mt-3 md:max-w-[70%]">
+                    Every program, every volunteer, and every tree we plant is living proof that collective
+                    action reshapes the environment one small step at a time.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,12 @@
 import HeroSection from '@/components/sections/HeroSection.vue'
 import ImpactSection from '@/components/sections/ImpactSection.vue'
 import PurposeSection from '@/components/sections/PurposeSection.vue'
+import UpcomingProgramSection from '@/components/sections/UpcomingProgramSection.vue'
 </script>
 
 <template>
   <HeroSection />
   <ImpactSection />
   <PurposeSection />
-  <div class="h-[150vh] bg-blue-100"></div>
+  <UpcomingProgramSection />
 </template>
