fd8705a51264b41350eceda6a3cb707d7baf6b02
EriyaDev <eriyagamedev@gmail.com>
2026-03-26 21:17:36 +0800

    update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -78,7 +78,7 @@ onUnmounted(() => {
           <div class="nav-link-underline"></div>
         </li>
         <li class="relative w-fit group">
-          <router-link class="nav-link" to="/blog">Blog</router-link>
+          <router-link class="nav-link" to="/blogs">Blog</router-link>
           <div class="nav-link-underline"></div>
         </li>
       </ul>
