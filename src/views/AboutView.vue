1e3e0c4cf75d8f24be26e4bd483ae30426fb67a1
EriyaDev <eriyagamedev@gmail.com>
2026-03-26 16:48:07 +0800

    add entrance animation

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,9 +7,22 @@ import WhoWeAreSection from '@/components/sections/about/WhoWeAreSection.vue'
 import BlogSection from '@/components/sections/BlogSection.vue'
 import CallToAction from '@/components/sections/CallToAction.vue'
 import Footer from '@/components/sections/Footer.vue'
+
+import { ref, onMounted } from 'vue'
+
+const isVisible = ref(false)
+
+onMounted(() => {
+  requestAnimationFrame(() => {
+    isVisible.value = true
+  })
+})
 </script>
 <template>
-  <div class="">
+  <div
+    class="transition-all duration-300"
+    :class="isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-6'"
+  >
     <AboutSection />
     <WhoWeAreSection />
     <WhereWeOperate />
