0eca290ce082bfee028797c73c5d76a56d23c517
EriyaDev <eriyagamedev@gmail.com>
2026-03-26 15:33:07 +0800

    faq section

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,14 @@
 <script setup>
 import AboutSection from '@/components/sections/about/AboutSection.vue'
+import TrustedBySection from '@/components/sections/about/TrustedBySection.vue'
+import WhereWeOperate from '@/components/sections/about/WhereWeOperate.vue'
 import WhoWeAreSection from '@/components/sections/about/WhoWeAreSection.vue'
 </script>
 <template>
   <div class="">
     <AboutSection />
     <WhoWeAreSection />
+    <WhereWeOperate />
+    <TrustedBySection />
   </div>
 </template>
