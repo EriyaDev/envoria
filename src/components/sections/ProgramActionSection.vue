5240ef994eb2e961d4a7a4d3a08a46ba46a4dad3
kusumadeveloper01 <idabagustrikusumadewa@gmail.com>
2026-03-26 20:53:15 +0800

    ganti content

--- src/components/sections/ProgramActionSection.vue
+++ src/components/sections/ProgramActionSection.vue
@@ -137,33 +137,27 @@ import 'swiper/css/pagination'
                         </span>
 
                         <div class="flex flex-col gap-2">
-                            <h2 class="small-heading font-medium">Borneo Forest Reforestation 2026</h2>
-                            <p class="text-small text-text-third-color line-clamp-2">A program to plant 10,000 endemic
-                                trees
-                                in critical lands of
-                                Central
-                                Kalimantan with
-                                local communities.</p>
+                            <h2 class="small-heading font-medium">Indonesian Waste Bank</h2>
+                            <p class="text-small text-text-third-color line-clamp-2">Expansion of the waste bank network
+                                to 100 new points in Java and Sulawesi.</p>
                         </div>
 
                         <div class="border border-dashed border-[#8E8E8E] flex flex-col gap-2 p-3 rounded-lg">
                             <span class="flex items-center gap-2">
                                 <img src="../../assets/icons/home/calendar.svg" alt="" />
-                                <span class="text-small text-text-third-color font-medium">15 Mar - 30 Jun
-                                    2026</span>
+                                <span class="text-small text-text-third-color font-medium">Jan - Dec 2026</span>
                             </span>
 
                             <div class="w-full">
                                 <div
                                     class="w-full h-3 bg-[#DBDBDB] rounded-full overflow-hidden border border-gray-200">
-                                    <div class="h-full bg-accent-color rounded-full" style="width: 87%"></div>
+                                    <div class="h-full bg-accent-color rounded-full" style="width: 85%"></div>
                                 </div>
                             </div>
 
                             <span class="flex items-center justify-between gap-2">
-                                <span class="text-small text-text-third-color font-medium">6.700 / 10.000
-                                    Tree</span>
-                                <span class="text-small text-text-third-color font-medium">67%</span>
+                                <span class="text-small text-text-third-color font-medium">149 / 200 Units</span>
+                                <span class="text-small text-text-third-color font-medium">85%</span>
                             </span>
                         </div>
 
@@ -173,7 +167,7 @@ import 'swiper/css/pagination'
                         </ButtonPrimary>
                     </div>
                     <div class="md:col-span-4 h-52 md:h-full w-full order-first md:order-last">
-                        <img src="../../assets/images/upcoming-program/image-1.svg" alt=""
+                        <img src="../../assets/images/upcoming-program/image-3.webp" alt=""
                             class="h-full w-full object-cover rounded-lg">
                     </div>
                 </div>
@@ -181,44 +175,41 @@ import 'swiper/css/pagination'
                 <div class="grid grid-cols-1 md:grid-cols-10 p-5 rounded-lg bg-primary-color gap-5">
                     <div class="md:col-span-6 gap-10 flex flex-col order-last md:order-first">
                         <span
-                            class="flex flex-row items-center w-fit px-3 py-1 bg-[#FFBB5B]/16 text-text-primary-color border border-[#61592B]/13 text-small rounded-full font-semibold">
-                            <div class="w-2 h-2 bg-[#8B1C00] rounded-full mr-2"></div>
-                            Coming Soon
+                            class="flex flex-row items-center w-fit px-3 py-1 bg-accent-color/16 text-text-primary-color border border-border-color/55 text-small rounded-full font-semibold">
+                            <div class="w-2 h-2 bg-accent-color rounded-full mr-2"></div>
+                            Finished
                         </span>
 
                         <div class="flex flex-col gap-2">
-                            <h2 class="small-heading font-medium">Clean-Up Pantai Bali 2026</h2>
-                            <p class="text-small text-text-third-color line-clamp-2">A mass beach cleanup took place in
-                                five
-                                locations in Bali. The goal was to remove 3 tons of plastic waste from the coast.</p>
+                            <h2 class="small-heading font-medium">Urban Farming Jakarta 2025</h2>
+                            <p class="text-small text-text-third-color line-clamp-2">An urban farming program in 50
+                                locations in Jakarta has successfully produced 2 tons of organic vegetables.</p>
                         </div>
 
                         <div class="border border-dashed border-[#8E8E8E] flex flex-col gap-2 p-3 rounded-lg">
                             <span class="flex items-center gap-2">
                                 <img src="../../assets/icons/home/calendar.svg" alt="" />
-                                <span class="text-small text-text-third-color font-medium">20 Mar - 30 July 2026</span>
+                                <span class="text-small text-text-third-color font-medium">Finished on dec 2025</span>
                             </span>
 
                             <div class="w-full">
                                 <div
                                     class="w-full h-3 bg-[#DBDBDB] rounded-full overflow-hidden border border-gray-200">
-                                    <div class="h-full bg-accent-color rounded-full" style="width: 0%"></div>
+                                    <div class="h-full bg-accent-color rounded-full" style="width: 100%"></div>
                                 </div>
                             </div>
 
                             <span class="flex items-center justify-between gap-2">
-                                <span class="text-small text-text-third-color font-medium">0 / 100 Units</span>
-                                <span class="text-small text-text-third-color font-medium">0%</span>
+                                <span class="text-small text-text-third-color font-medium">50 / 50 Units</span>
+                                <span class="text-small text-text-third-color font-medium">100%</span>
                             </span>
                         </div>
 
-                        <ButtonPrimary class="w-fit">
-                            <p class="text-body font-medium">Sign Up Now</p>
-                            <img src="../../assets/icons/home/arrow-black.svg" alt="" />
-                        </ButtonPrimary>
+                        <button class="button-outline flex items-center gap-2 w-fit">See Report <img
+                                src="../../assets/icons/home/arrow-black.svg" alt=""></button>
                     </div>
                     <div class="md:col-span-4 h-52 md:h-full w-full order-first md:order-last">
-                        <img src="../../assets/images/upcoming-program/image-2.svg" alt=""
+                        <img src="../../assets/images/upcoming-program/image-4.webp" alt=""
                             class="h-full w-full object-cover rounded-lg">
                     </div>
                 </div>
--- src/components/sections/ProgramCrowdfundingSection.vue
+++ src/components/sections/ProgramCrowdfundingSection.vue
@@ -28,51 +28,54 @@ import 'swiper/css/pagination'
             <div class="grid grid-cols-1 mt-10 xl:grid-cols-2 gap-5">
                 <div class="grid xl:col-span-2 grid-cols-1 md:grid-cols-10 p-5 rounded-lg bg-primary-color gap-5">
                     <div class="md:col-span-5 h-100 w-full">
-                        <img src="../../assets/images/upcoming-program/image-1.svg" alt=""
+                        <img src="../../assets/images/program-crowdfunding/image-1.webp" alt=""
                             class="h-full w-full object-cover rounded-lg">
                     </div>
 
                     <div class="md:col-span-5 gap-10 flex flex-col">
-                        <span
-                            class="flex flex-row items-center w-fit px-3 py-1 bg-accent-color/16 text-text-primary-color border border-border-color/55 text-small rounded-full font-semibold">
-                            <div class="w-2 h-2 bg-accent-color rounded-full mr-2"></div>
-                            Active Now
-                        </span>
+                        <div class="flex items-center gap-5">
+                            <span
+                                class="flex flex-row items-center w-fit px-3 py-1 bg-[#FFF3F3] text-[#FF6666] border border-[#6C0000]/55 text-small rounded-full font-semibold">
+                                <div class="w-2 h-2 bg-[#FF3030] rounded-full mr-2"></div>
+                                ACTIVE - Running Now
+                            </span>
+                            <span
+                                class="flex flex-row items-center w-fit px-3 py-1 bg-[#FFF3F3] text-[#FF6666] border border-[#6C0000]/55 text-small rounded-full font-semibold">
+                                <div class="w-2 h-2 bg-[#FF3030] rounded-full mr-2"></div>
+                                3 Days Left
+                            </span>
+                        </div>
 
                         <div class="flex flex-col gap-2">
-                            <h2 class="small-heading font-medium">Borneo Forest Reforestation 2026</h2>
-                            <p class="text-small text-text-third-color line-clamp-2">A program to plant 10,000 endemic
-                                trees
-                                in critical lands of
-                                Central
-                                Kalimantan with
-                                local communities.</p>
+                            <h2 class="small-heading font-medium">Emergency Fund for Forest Recovery After the Riau
+                                Fires</h2>
+                            <p class="text-small text-text-third-color line-clamp-2">More than 2,000 hectares of Riau's
+                                peatland forests have burned. EcoNusa is launching an emergency fundraising campaign to
+                                cover land rehabilitation costs and the recovery of affected wildlife.</p>
                         </div>
 
                         <div class="border border-dashed border-[#8E8E8E] flex flex-col gap-2 p-3 rounded-lg">
                             <span class="flex items-center gap-2">
-                                <img src="../../assets/icons/home/calendar.svg" alt="" />
-                                <span class="text-small text-text-third-color font-medium">15 Mar - 30 Jun
-                                    2026</span>
+                                <img src="../../assets/icons/home/group-outline-2.svg" alt="" />
+                                <span class="text-small text-text-third-color font-medium">+1,236 others joined</span>
                             </span>
 
                             <div class="w-full">
                                 <div
                                     class="w-full h-3 bg-[#DBDBDB] rounded-full overflow-hidden border border-gray-200">
-                                    <div class="h-full bg-accent-color rounded-full" style="width: 87%"></div>
+                                    <div class="h-full bg-accent-color rounded-full" style="width: 76%"></div>
                                 </div>
                             </div>
 
                             <span class="flex items-center justify-between gap-2">
-                                <span class="text-small text-text-third-color font-medium">6.700 / 10.000
-                                    Tree</span>
-                                <span class="text-small text-text-third-color font-medium">67%</span>
+                                <span class="text-small text-text-third-color font-medium">$25,677.87 / $30,000</span>
+                                <span class="text-small text-text-third-color font-medium">76%</span>
                             </span>
                         </div>
 
                         <ButtonPrimary class="w-fit">
-                            <p class="text-body font-medium">Sign Up Now</p>
-                            <img src="../../assets/icons/home/arrow-black.svg" alt="" />
+                            <img src="../../assets/icons/home/heart.svg" alt="" />
+                            <p class="text-body font-medium">Donate Now</p>
                         </ButtonPrimary>
                     </div>
                 </div>
@@ -80,44 +83,44 @@ import 'swiper/css/pagination'
                 <div class="grid grid-cols-1 md:grid-cols-10 p-5 rounded-lg bg-primary-color gap-5">
                     <div class="md:col-span-6 gap-10 flex flex-col order-last md:order-first">
                         <span
-                            class="flex flex-row items-center w-fit px-3 py-1 bg-[#FFBB5B]/16 text-text-primary-color border border-[#61592B]/13 text-small rounded-full font-semibold">
-                            <div class="w-2 h-2 bg-[#8B1C00] rounded-full mr-2"></div>
-                            Coming Soon
+                            class="flex flex-row items-center w-fit px-3 py-1 bg-accent-color/16 text-text-primary-color border border-border-color/55 text-small rounded-full font-semibold">
+                            <div class="w-2 h-2 bg-accent-color rounded-full mr-2"></div>
+                            Active Now
                         </span>
 
                         <div class="flex flex-col gap-2">
-                            <h2 class="small-heading font-medium">Clean-Up Pantai Bali 2026</h2>
-                            <p class="text-small text-text-third-color line-clamp-2">A mass beach cleanup took place in
-                                five
-                                locations in Bali. The goal was to remove 3 tons of plastic waste from the coast.</p>
+                            <h2 class="small-heading font-medium">Procurement of Professional Beach Cleaning Equipment
+                            </h2>
+                            <p class="text-small text-text-third-color line-clamp-2">Purchased nets, gloves, and safety
+                                equipment for 200 beach clean-up volunteers.</p>
                         </div>
 
                         <div class="border border-dashed border-[#8E8E8E] flex flex-col gap-2 p-3 rounded-lg">
                             <span class="flex items-center gap-2">
                                 <img src="../../assets/icons/home/calendar.svg" alt="" />
-                                <span class="text-small text-text-third-color font-medium">20 Mar - 30 July 2026</span>
+                                <span class="text-small text-text-third-color font-medium">Jan - Dec 2026</span>
                             </span>
 
                             <div class="w-full">
                                 <div
                                     class="w-full h-3 bg-[#DBDBDB] rounded-full overflow-hidden border border-gray-200">
-                                    <div class="h-full bg-accent-color rounded-full" style="width: 0%"></div>
+                                    <div class="h-full bg-accent-color rounded-full" style="width: 85%"></div>
                                 </div>
                             </div>
 
                             <span class="flex items-center justify-between gap-2">
-                                <span class="text-small text-text-third-color font-medium">0 / 100 Units</span>
-                                <span class="text-small text-text-third-color font-medium">0%</span>
+                                <span class="text-small text-text-third-color font-medium">$1,230.00 / $1,600.00</span>
+                                <span class="text-small text-text-third-color font-medium">85%</span>
                             </span>
                         </div>
 
                         <ButtonPrimary class="w-fit">
-                            <p class="text-body font-medium">Sign Up Now</p>
-                            <img src="../../assets/icons/home/arrow-black.svg" alt="" />
+                            <img src="../../assets/icons/home/heart.svg" alt="" />
+                            <p class="text-body font-medium">Donate Now</p>
                         </ButtonPrimary>
                     </div>
                     <div class="md:col-span-4 h-52 md:h-full w-full order-first md:order-last">
-                        <img src="../../assets/images/upcoming-program/image-2.svg" alt=""
+                        <img src="../../assets/images/upcoming-program/image-3.webp" alt=""
                             class="h-full w-full object-cover rounded-lg">
                     </div>
                 </div>
@@ -131,43 +134,38 @@ import 'swiper/css/pagination'
                         </span>
 
                         <div class="flex flex-col gap-2">
-                            <h2 class="small-heading font-medium">Borneo Forest Reforestation 2026</h2>
-                            <p class="text-small text-text-third-color line-clamp-2">A program to plant 10,000 endemic
-                                trees
-                                in critical lands of
-                                Central
-                                Kalimantan with
-                                local communities.</p>
+                            <h2 class="small-heading font-medium">Raja Ampat Coral Reef Seeds
+                            </h2>
+                            <p class="text-small text-text-third-color line-clamp-2">Purchase of 500 coral reef
+                                fragments from an official nursery for an underwater rehabilitation program.</p>
                         </div>
 
                         <div class="border border-dashed border-[#8E8E8E] flex flex-col gap-2 p-3 rounded-lg">
                             <span class="flex items-center gap-2">
                                 <img src="../../assets/icons/home/calendar.svg" alt="" />
-                                <span class="text-small text-text-third-color font-medium">15 Mar - 30 Jun
-                                    2026</span>
+                                <span class="text-small text-text-third-color font-medium">Jan - Dec 2026</span>
                             </span>
 
                             <div class="w-full">
                                 <div
                                     class="w-full h-3 bg-[#DBDBDB] rounded-full overflow-hidden border border-gray-200">
-                                    <div class="h-full bg-accent-color rounded-full" style="width: 87%"></div>
+                                    <div class="h-full bg-accent-color rounded-full" style="width: 85%"></div>
                                 </div>
                             </div>
 
                             <span class="flex items-center justify-between gap-2">
-                                <span class="text-small text-text-third-color font-medium">6.700 / 10.000
-                                    Tree</span>
-                                <span class="text-small text-text-third-color font-medium">67%</span>
+                                <span class="text-small text-text-third-color font-medium">$1,230.00 / $1,600.00</span>
+                                <span class="text-small text-text-third-color font-medium">85%</span>
                             </span>
                         </div>
 
                         <ButtonPrimary class="w-fit">
-                            <p class="text-body font-medium">Sign Up Now</p>
-                            <img src="../../assets/icons/home/arrow-black.svg" alt="" />
+                            <img src="../../assets/icons/home/heart.svg" alt="" />
+                            <p class="text-body font-medium">Donate Now</p>
                         </ButtonPrimary>
                     </div>
                     <div class="md:col-span-4 h-52 md:h-full w-full order-first md:order-last">
-                        <img src="../../assets/images/upcoming-program/image-1.svg" alt=""
+                        <img src="../../assets/images/upcoming-program/image-3.webp" alt=""
                             class="h-full w-full object-cover rounded-lg">
                     </div>
                 </div>
