e5df8a3d2213cc75bb08bdf8ae52dde06cd8c78b
EriyaDev <eriyagamedev@gmail.com>
2026-03-26 15:42:20 +0800

    about view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,12 @@
 <script setup>
 import AboutSection from '@/components/sections/about/AboutSection.vue'
+import FaqSection from '@/components/sections/about/FaqSection.vue'
 import TrustedBySection from '@/components/sections/about/TrustedBySection.vue'
 import WhereWeOperate from '@/components/sections/about/WhereWeOperate.vue'
 import WhoWeAreSection from '@/components/sections/about/WhoWeAreSection.vue'
+import BlogSection from '@/components/sections/BlogSection.vue'
+import CallToAction from '@/components/sections/CallToAction.vue'
+import Footer from '@/components/sections/Footer.vue'
 </script>
 <template>
   <div class="">
@@ -10,5 +14,9 @@ import WhoWeAreSection from '@/components/sections/about/WhoWeAreSection.vue'
     <WhoWeAreSection />
     <WhereWeOperate />
     <TrustedBySection />
+    <FaqSection />
+    <BlogSection />
+    <CallToAction />
+    <Footer />
   </div>
 </template>
